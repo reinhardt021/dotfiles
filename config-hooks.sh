6b5766f60282af67a76e2d02bcf873c2db205fa3
reinhardtcgr <reinhardr.cgr@gmail.com>
2021-04-18 19:35:55 +1200

    made the commit hooks trackable by the repo

---
+++ config-hooks.sh
@@ -0,0 +1,3 @@
+#cant run this script so wil have to run in command line if possible
+cp ~/dotfiles/git-hooks/pre-commit ~/dotfiles/.git/hooks/pre-commit;
+#ln ~/dotfiles/git-hooks/pre-commit ~/dotfiles/.git/hooks/pre-commit;
