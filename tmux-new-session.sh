beec5f666eb7c6f6bcc784b668182e792512be42
Reinhardt <reinhardt.cgr@gmail.com>
2021-05-03 22:20:06 +1200

    changed indexes since we start at one now to make it easier to change between windows

--- tmux-new-session.sh
+++ tmux-new-session.sh
@@ -5,23 +5,23 @@ echo "That's fine. Lets create a new session.\n"
 #// with first window called cmd (aka command center)
 tmux new -s newS -n cmd -d;
     #// split the 'command' window into a top and bottom pane (vertical split) 
-    tmux split-window -v -t newS:0;
+    tmux split-window -v -t newS:1;
     #// split the 'command' window's top pane into a left & right pane (horizontal split) at 40/60 ratio
-    tmux split-window -h -l 60% -t newS:0.0;
+    tmux split-window -h -l 60% -t newS:1.1;
     #// TODO: figure out why the resize is not working for the panes
 
     #// create a new window for coding called 'CODE' (at window index 1 on session 'ps')
-    tmux new-window -t newS:1 -n CODE
+    tmux new-window -t newS:2 -n CODE
 
     #// create a new window for source control called 'git' (at window index 2 on session 'ps')
-    tmux new-window -t newS:2 -n git
+    tmux new-window -t newS:3 -n git
     #// split the window into a left and right pane at 27/73 ratio
-    tmux split-window -h -l 73% -t newS:2
+    tmux split-window -h -l 73% -t newS:3
     #// split the left pane into a top and bottom pane at 30/70 ratio
-    tmux split-window -v -l 70% -t newS:2.0
+    tmux split-window -v -l 70% -t newS:3.1
     
     #// create a new window as a sandbax / tinkering / misc
-    tmux new-window -t newS:3 -n misc 
+    tmux new-window -t newS:4 -n misc 
 fi
 echo "the new TMUX session (newS) should be good to attach to now:"
 echo "tmux attach -t <session-name>\n"
