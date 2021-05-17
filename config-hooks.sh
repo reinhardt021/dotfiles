a63ae58de6694b4643d07e5c808133bea206bbdf
Reinhardt <reinhardt.cgr@gmail.com>
2021-05-17 13:56:55 +1200

    clarified my notes a bit in the git hooks script

--- config-hooks.sh
+++ config-hooks.sh
@@ -1,3 +1,4 @@
-#cant run this script so wil have to run in command line if possible
+#cant run this symlink script so wil have to run in command line if possible
+# will have to just have to run copy for now
 #ln ~/dotfiles/git-hooks/pre-commit ~/dotfiles/.git/hooks/pre-commit;
 cp ~/dotfiles/git-hooks/pre-commit ~/dotfiles/.git/hooks/pre-commit;
