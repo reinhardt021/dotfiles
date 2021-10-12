dff94890c84edffec4bfa897ee7ab00ecf4b1407
Reinhardt <reinhardt.cgr@gmail.com>
2021-10-13 08:51:53 +1300

    changed session names to something simpler

--- tmux-new-session.sh
+++ tmux-new-session.sh
@@ -42,11 +42,12 @@ tmux new -s $SESSION_ID -n cmd -d;
     
     #// WINDOW: CODE
     #// create a new window for coding called 'CODE' (at window index 1 on session 'ps')
-    tmux new-window -t $SESSION_ID:4 -n CODE
+    #tmux new-window -t $SESSION_ID:4 -n CODE
+    tmux new-window -t $SESSION_ID:4 -n A
 
     #// WINDOW: MISC
     #// create a new window as a sandbax / tinkering / misc
-    tmux new-window -t $SESSION_ID:5 -n x 
+    tmux new-window -t $SESSION_ID:5 -n b 
 fi
 echo "The new TMUX session ($SESSION_ID) is ready to ATTACH to now:"
 echo "$ tmux attach -t <session-name>"
