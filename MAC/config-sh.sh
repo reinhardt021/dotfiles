6ae74fede413d72b56baecc30886e3e32f4af54f
Reinhardt <reinhardt.cgr@gmail.com>
2022-04-08 08:54:28 +1200

    changed to use .zshrc since .zprofile didn't seem to load for iterm on initial open

--- MAC/config-sh.sh
+++ MAC/config-sh.sh
@@ -6,7 +6,8 @@
 # // if so then add to the end of the files
 # // if no then do the straight forward symlink
 #ln -s ~/dotfiles/MAC/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/MAC/.zprofile ~/.zprofile
+#ln -s ~/dotfiles/MAC/.zprofile ~/.zprofile # this doesn't seem to load when opening new terminal sessions
+ln -s ~/dotfiles/MAC/.zshrc ~/.zshrc
 # // note: use path relative to home (~/.../file) 
 # // not relative to pwd (./file) 
 
