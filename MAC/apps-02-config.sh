b94e6e5744d63fd36a8c10184a83c42831485bfe
Reinhardt <reinhardt.cgr@gmail.com>
2022-04-07 23:32:12 +1200

    change to use zsh now

--- MAC/apps-02-config.sh
+++ MAC/apps-02-config.sh
@@ -2,6 +2,7 @@
 
 #// TODO: check for dependencies needed to run these configs
 #// TODO: call the other sh files
-sh ./config-bash.sh
+#sh ./config-bash.sh
+sh ./config-sh.sh
 sh ~/dotfiles/config-tmux.sh
 sh ~/dotfiles/config-vim.sh
--- MAC/config-sh.sh
+++ MAC/config-sh.sh
@@ -5,7 +5,8 @@
 # // TODO: for each of these files check if files already exists 
 # // if so then add to the end of the files
 # // if no then do the straight forward symlink
-ln -s ~/dotfiles/MAC/.bash_profile ~/.bash_profile
+#ln -s ~/dotfiles/MAC/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/MAC/.zprofile ~/.zprofile
 # // note: use path relative to home (~/.../file) 
 # // not relative to pwd (./file) 
 
