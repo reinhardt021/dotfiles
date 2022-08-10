29f13b38db381ba728da4322858023781d3bb4d4
Reinhardt <reinhardt.cgr@gmail.com>
2022-08-10 21:20:53 +1200

    fixed the ownership to allow execution on new laptop setup

--- MAC/apps-02-config.sh
+++ MAC/apps-02-config.sh
@@ -3,6 +3,6 @@
 #// TODO: check for dependencies needed to run these configs
 #// TODO: call the other sh files
 #sh ./config-bash.sh
-sh ./config-sh.sh
+sh ~/dotfiles/MAC/config-sh.sh
 sh ~/dotfiles/config-tmux.sh
 sh ~/dotfiles/config-vim.sh
