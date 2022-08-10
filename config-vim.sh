1ba79766ac528c4ba29663cad283ea089a5f9d9c
Reinhardt <reinhardt.cgr@gmail.com>
2022-08-10 21:19:23 +1200

    modify VIM instructions for plugins which seem to auto install

--- config-vim.sh
+++ config-vim.sh
@@ -9,6 +9,6 @@ mkdir -p ~/.config/nvim;
 # // TODO: for each of these files check if files already exists 
 # // if so then add to the end of the files
 # // if no then do the straight forward symlink
-# // TODO: install VIM plugins 
+# // TODO: install VIM plugins - might also auto install when first run
 ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim;
 
