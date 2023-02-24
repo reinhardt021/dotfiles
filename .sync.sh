541f27b7caa25c46d05a130f925f822628f98b19
Reinhardt <reinhardt.cgr@gmail.com>
2023-02-24 00:09:13 -0800

    dotfiles sync

--- .sync.sh
+++ .sync.sh
@@ -1,14 +1,24 @@
 #echo "test"
+echo "// git status"
 git status
+echo "// git add ."
 git add .
+echo "// git status"
 git status
+echo "// git commit -m 'dotfiles sync'"
 git commit -m "dotfiles sync"
 
+echo "// git fetch"
 git fetch
+echo "// git rebase origin/main"
 git rebase origin/main
 
+echo "// git log --graph --name-only --oneline -n 2"
 git log --graph --name-only --oneline -n 2
+
+echo "// git push origin main"
 git push origin main
+echo "// git log --graph --name-only --oneline -n 2"
 git log --graph --name-only --oneline -n 2
 
 
