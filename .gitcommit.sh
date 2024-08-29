a85a760221e2baee0db538a4ff18b0cd89fb5952
Reinhardt <reinhardt.cgr@gmail.com>
2024-08-29 12:40:56 -0700

    : cleanup

--- .gitcommit.sh
+++ .gitcommit.sh
@@ -7,19 +7,15 @@ separator=":"
 last_commit=$(git log -1 --pretty=%B)
 if [[ "$last_commit" == *"$separator"* ]]; then
   IFS="$separator" read -r first_word _ <<< "$last_commit" 
-  #echo "first word is: $first_word"
 fi
 
 prefix_prompt="___ADD A PREFIX?___"
 echo "$prefix_prompt"
 if [[ -z "$first_word" ]]; then
-  #if empty
   read prefix
 else
   echo "$first_word ? [Y=enter]"
   read prefix
-  #read -e -p "$prefix_prompt" -i "$first_word" prefix #BAD
-  #read -e -p "$prefix_prompt\n $first_word?[Y=enter]" prefix
 fi
 
 if [ "$prefix" != "" ]
