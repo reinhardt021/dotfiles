7ccd6f9478fc7b00cbcccef24c6636bc6ab70227
Reinhardt <reinhardt.cgr@gmail.com>
2024-10-01 16:51:51 -0700

    fixed script to target proper folder

--- os-LINUX-remote/config-bash.sh
+++ os-LINUX-remote/config-bash.sh
@@ -5,7 +5,7 @@
 # // TODO: for each of these files check if files already exists 
 # // if so then add to the end of the files
 # // if no then do the straight forward symlink
-ln -s ~/dotfiles/os-LINUX/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/os-LINUX-remote/.bash_profile ~/.bash_profile
 # // note: use path relative to home (~/.../file) 
 # // not relative to pwd (./file) 
 
